6114a2ae69f6ab228929b728507bd560f5d7f0d7
Chamika196 <chamikarathnayaka1996@gmail.com>
2024-01-06 16:59:19 +0530

    inital commit

---
+++ src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { AddBlogPost } from '../models/add-blog-post.model';
+import { BlogPostService } from '../services/blog-post.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-add-blogpost',
+  templateUrl: './add-blogpost.component.html',
+  styleUrls: ['./add-blogpost.component.css']
+})
+export class AddBlogpostComponent {
+
+  model: AddBlogPost;
+
+  constructor(private blogPostServices: BlogPostService,
+    private router: Router){
+    this.model = {
+      title:'',
+      shortDescription:'',
+      urlHandle:'',
+      content:'',
+      featuredImageUrl:'',
+      author:'',
+      isVisible:true,
+      publishedDate: new Date()
+    }
+  }
+
+  onFormSubmit():void{
+    this.blogPostServices.createBlogPost(this.model)
+    .subscribe({
+      next: (response) => {
+        this.router.navigateByUrl('/admin/blogposts');
+      }
+    });
+  }
+
+
+}
---
+++ src/app/features/blog-post/blogpost-list/blogpost-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BlogPost } from '../models/blog-post.model';
+import { BlogPostService } from '../services/blog-post.service';
+
+@Component({
+  selector: 'app-blogpost-list',
+  templateUrl: './blogpost-list.component.html',
+  styleUrls: ['./blogpost-list.component.css']
+})
+export class BlogpostListComponent implements OnInit {
+
+  blogPost$?: Observable<BlogPost[]>;
+
+  constructor(private blogPostService: BlogPostService){
+
+  }
+
+  ngOnInit(): void {
+    //get all blog posts from API
+
+    this.blogPost$ = this.blogPostService.getAllBlogPosts();
+  }
+}
